ff8c0aae39a7e4d18b5b218e3274a5dec679e50b
Mithrareddy08 <sangamithravanga@gmail.com>
2023-07-19 10:41:29 -0500

    spring

---
+++ src/main/java/com/example/demo/service/SubmissionServiceImp.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import com.example.demo.SubmissionDTO;
+import com.example.demo.repository.SubmissionRepoClass;
+
+import com.example.demo.service.SubmissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubmissionServiceImp implements SubmissionService {
+    @Autowired
+    private SubmissionRepoClass submissionRepository;
+
+
+
+    @Override
+    public SubmissionDTO postSubmission(SubmissionDTO sf) {
+        return submissionRepository.postSubmission(sf);
+    }
+
+    @Override
+    public SubmissionDTO getSubmissionFormById(String Id) {
+        return submissionRepository.getSubmissionFormById(Id);
+    }
+
+    @Override
+    public List<SubmissionDTO> getallSubmissions() {
+        return submissionRepository.getallSubmissions();
+    }
+
+    @Override
+    public SubmissionDTO editSubmission(SubmissionDTO sf) {
+        return submissionRepository.editSubmission(sf);
+    }
+
+    @Override
+    public List<SubmissionDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<SubmissionDTO> deleteSubmission(String Id) {
+        return submissionRepository.deleteSubmission(Id);
+    }
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import com.example.demo.service.SubmissionService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+//    @Mock
+//	private SubmissionService service;
+	@Test
+	void contextLoads() {
+	}
+}
